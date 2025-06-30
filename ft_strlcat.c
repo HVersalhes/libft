cc664bdff7b946206a4319fd59679f5ad6f551fb
Hcosta <helderversalhes@gmail.com>
2025-06-30 17:09:06 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,5 +1,15 @@
 #include "libft.h"
-
+/**
+ * ft_strlcat - Concatena a string src à string dst, limitando o tamanho total a dstsize.
+ * 
+ * @dst:     String de destino (deve ter espaço suficiente para receber src).
+ * @src:     String de origem.
+ * @dstsize: Tamanho total do buffer de destino (incluindo o '\0').
+ *
+ * Retorna o comprimento total que a string concatenada teria (comprimento inicial de dst + comprimento de src).
+ * Garante que dst sempre estará terminada por '\0' se dstsize > 0.
+ * Não concatena mais do que (dstsize - strlen(dst) - 1) bytes de src.
+ */
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	src_len;
