31f5d69dda40c5eb75a2381b87f15d6e705e975a
HVersalhes <helderversalhes@gmail.com>
2026-08-02 01:04:03 +0100

    ficheiros deletados do repositorio

--- ft_isalpha.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int	ft_isalpha(int c)
-{
-	unsigned char	uc;
-
-	uc = (unsigned char)c;
-	return ((uc >= 'A' && uc <= 'Z') || (uc >= 'a' && uc <= 'z'));
-}
--- ft_isdigit.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int	ft_isdigit(int c)
-{
-	unsigned char	uc;
-
-	uc = (unsigned char)c;
-	return (uc >= '0' && uc <= '9');
-}
