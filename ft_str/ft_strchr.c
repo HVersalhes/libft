2ec65618cb4bf90e6da4c156a51b9f637e5f2074
HVersalhes <helderversalhes@gmail.com>
2026-08-03 00:34:59 +0100

    atualização do ft_strchr

--- ft_str/ft_strchr.c
+++ ft_str/ft_strchr.c
@@ -1,16 +1,5 @@
 #include "libft.h"
 
-/*
- * ft_strchr
- * -----------
- * Procura a primeira ocorrência do caractere 'c' na string 's'.
- * Retorna um ponteiro para a posição do caractere encontrado na string.
- * Se o caractere não for encontrado, retorna NULL.
- * Se 'c' for '\0', retorna ponteiro para o final da string.
- * Protege contra ponteiro nulo, retornando NULL se 's' for NULL.
- */
-
-
 char	*ft_strchr(const char *s, int c)
 {
 	if (!s)
